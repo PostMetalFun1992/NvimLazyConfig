a7ca6ae0aea9b463f5689afa00efaf4cb8f518b8
PostMetalFun1992 <kabanovkirill54@protonmail.com>
2025-04-15 16:25:34 +0500

    temporary removed lsp confs

--- lua/postmetalfun/lazy.lua
+++ lua/postmetalfun/lazy.lua
@@ -15,7 +15,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup({ { import = "postmetalfun.plugins" }, { import = "postmetalfun.lsp" } }, {
+require("lazy").setup({ import = "postmetalfun.plugins" }, {
   change_detection = {
     notify = false,
   },
