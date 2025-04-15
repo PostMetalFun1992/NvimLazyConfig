4c402d2e35877f5d8e068716b885e900fa5efc0e
PostMetalFun1992 <kabanovkirill54@protonmail.com>
2025-04-15 16:26:01 +0500

    removed unnecessary dependency from autopairs

--- lua/postmetalfun/plugins/autopairs.lua
+++ lua/postmetalfun/plugins/autopairs.lua
@@ -1,9 +1,6 @@
 return {
   "windwp/nvim-autopairs",
   event = { "InsertEnter" },
-  dependencies = {
-    "hrsh7th/nvim-cmp",
-  },
   config = function()
     local autopairs = require("nvim-autopairs")
     autopairs.setup({ check_ts = true })
