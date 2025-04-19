f5156874603515381f38edaafc8491d8da3044f2
PostMetalFun1992 <kabanovkirill54@protonmail.com>
2025-04-20 02:16:53 +0500

    set up conform with local settings

--- local_confs/python3.nvim.lua
+++ local_confs/python3.nvim.lua
@@ -2,7 +2,24 @@ local env_path = vim.fn.getcwd() .. "/.venv"
 
 vim.g.python3_host_prog = env_path .. "/bin/python3"
 
--- Don't forget to install pynvim + python-lsp-server
+-- Install pynvim + python-lsp-server
 require('lspconfig').pylsp.setup({
   cmd = { env_path .. "/bin/pylsp" },
+})
+
+-- Install ruff
+require("conform").setup({
+  formatters = {
+    ruff = {
+      command = env_path .. "/bin/ruff",
+    },
+  },
+  formatters_by_ft = {
+    python = { "ruff" },
+  },
+  format_on_save = {
+    lsp_fallback = true,
+    async = false,
+    timeout_ms = 1000,
+  },
 })
