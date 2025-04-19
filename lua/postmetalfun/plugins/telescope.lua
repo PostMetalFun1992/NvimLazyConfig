a5f2d8e1cd481a65406c308cfa40a573afcba686
PostMetalFun1992 <kabanovkirill54@protonmail.com>
2025-04-20 02:39:50 +0500

    always open telescope results in the new tab

--- lua/postmetalfun/plugins/telescope.lua
+++ lua/postmetalfun/plugins/telescope.lua
@@ -17,6 +17,10 @@ return {
           i = {
             ["<C-k>"] = actions.move_selection_previous, -- move to prev result
             ["<C-j>"] = actions.move_selection_next, -- move to next result
+            ["<CR>"] = actions.select_tab,
+          },
+          n = {
+            ["<CR>"] = actions.select_tab,
           },
         },
       },
